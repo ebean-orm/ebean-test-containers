b03b06f0ab859a5db206bcd6a98e10b2e64403cc
Rob Bygrave <robin.bygrave@gmail.com>
2018-02-02 21:09:27 +1300

    #11 - Oracle - grant create user ... to the user being created

--- src/main/java/org/avaje/docker/commands/OracleContainer.java
+++ src/main/java/org/avaje/docker/commands/OracleContainer.java
@@ -170,7 +170,7 @@ public class OracleContainer extends DbContainer implements Container {
         runSql(connection, "drop user " + dbConfig.getDbUser() + " cascade");
       }
       runSql(connection, "create user " + dbConfig.getDbUser() + " identified by " + dbConfig.getDbPassword());
-      runSql(connection, "grant connect, resource, unlimited tablespace to " + dbConfig.getDbUser());
+      runSql(connection, "grant create view, connect, resource, unlimited tablespace to " + dbConfig.getDbUser());
     });
     return true;
   }
