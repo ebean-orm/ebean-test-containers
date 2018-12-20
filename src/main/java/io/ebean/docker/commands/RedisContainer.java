9e7d81a859d715e3eb72e5092989a39a06e0c498
rob bygrave <robin.bygrave@gmail.com>
2018-12-20 21:14:07 +1300

    #34 - ENH: Add support for Redis container

--- src/main/java/io/ebean/docker/commands/RedisContainer.java
+++ src/main/java/io/ebean/docker/commands/RedisContainer.java
@@ -1,10 +1,5 @@
 package io.ebean.docker.commands;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,22 +33,12 @@ public class RedisContainer extends BaseContainer {
 
       Process process = pb.start();
 
-      OutputStream in = process.getOutputStream();
-      InputStream out = process.getInputStream();
-
-      StreamGobbler streamGobbler = new StreamGobbler(out);
-      streamGobbler.start();
-
-      OutputStreamWriter ow = new OutputStreamWriter(in);
+      OutputStreamWriter ow = new OutputStreamWriter(process.getOutputStream());
       ow.write("PING");
       ow.flush();
       ow.close();
 
-      int status = process.waitFor();
-
-      log.trace("status:{} output:{}", status, streamGobbler.out());
-
-      return status == 0;
+      return process.waitFor() == 0;
 
     } catch (Exception e) {
       e.printStackTrace();
@@ -86,33 +71,4 @@ public class RedisContainer extends BaseContainer {
     return createProcessBuilder(args);
   }
 
-  static class StreamGobbler extends Thread {
-
-    private final InputStream is;
-
-    private final StringBuilder out = new StringBuilder();
-
-    StreamGobbler(InputStream is) {
-      this.is = is;
-    }
-
-    String out() {
-      return out.toString();
-    }
-
-    public void run() {
-      try {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-        String line;
-        while ((line = br.readLine()) != null) {
-          out.append(line).append(" ");
-        }
-
-      } catch (IOException e) {
-        log.error("Error reading output stream", e);
-      }
-    }
-  }
 }
