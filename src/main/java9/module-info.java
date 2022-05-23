d011965f7a207bc50c5295fccf546216057f013b
Rob Bygrave <robin.bygrave@gmail.com>
2022-05-23 20:56:28 +1200

    Bump to 5.8-SNAPSHOT

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -3,7 +3,6 @@ open module io.ebean.docker {
   requires transitive org.slf4j;
   requires transitive java.sql;
 
-  exports io.ebean.docker.commands;
-  exports io.ebean.docker.container;
+  exports io.ebean.test.containers;
 
 }
